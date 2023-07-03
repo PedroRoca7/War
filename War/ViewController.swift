2a3a15a8fc951a2bcb82701afecfdfdb12eef66f
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2023-07-03 19:21:03 -0300

    Initial Commit

---
+++ War/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by Pedro Henrique on 03/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
