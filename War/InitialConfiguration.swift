a92c6f8297966ab074cfc70971fe7f373eb34505
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2023-07-03 21:12:40 -0300

    Commit Criando um projeto para jogar War
    Criando um projeto para jogar o jogo de tabuleiro War, a ideia é fazer de forma automática os cálculos de quantos soldados os jogadores vão colocar em seus exércitos a cada rodada.

---
+++ War/InitialConfiguration.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by Pedro Henrique on 03/07/23.
+//
+
+import UIKit
+
+class InitialConfiguration: UIViewController {
+
+    @IBOutlet var buttonsColorsSelected: [UIButton]!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func selectColor(_ sender: UIButton) {
+        let index = buttonsColorsSelected.firstIndex(of: sender)
+        performSegue(withIdentifier: "segue", sender: index)
+        func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+             if segue.identifier == "segue" {
+                 let ic = segue.destination as! ManagerController
+                 ic.numbColor = index
+             }
+         }
+    }
+    
+}
+
---
+++ War/ManagerController.swift
@@ -0,0 +1,31 @@
+//
+//  ManagerController.swift
+//  War
+//
+//  Created by Pedro Henrique on 03/07/23.
+//
+
+import UIKit
+
+class ManagerController: UIViewController {
+    
+    var numbColor: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
