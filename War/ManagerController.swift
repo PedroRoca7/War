6595b0b672af0587fab8857692b908e38c271364
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2023-07-07 17:26:56 -0300

    Commit Adicionado imagens e alert inicial
    Foi adicionado as imagens das cores dos jogadores, e também criado o alert quando aparece a tela de informações para que o jogador informe a quantidade de territórios iniciais.

--- War/ManagerController.swift
+++ War/ManagerController.swift
@@ -16,17 +16,24 @@ class ManagerController: UIViewController {
     @IBOutlet var switchConqueredContinents: [UISwitch]!
     
     var numbColor: Int?
+    var numberTerritories: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let numberColor = numbColor {
-            labelTotalTerritoriesConquered.text = convertIntToString(number: numberColor)
-        }
+        colorPlayer(number: numbColor ?? 0)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        exibirPopUp()
     }
     
     func convertIntToString(number: Int) -> String {
-        let convert = String(number)
-        return convert
+        if number.isMultiple(of: 2) {
+            return String(number)
+        } else {
+            let resultArredondadoParaBaixo = Int(floor(Double(number)))
+            return String(resultArredondadoParaBaixo)
+        }
     }
     
     func convertStringToInt(text: String) -> Int {
@@ -37,5 +44,74 @@ class ManagerController: UIViewController {
     @IBAction func updateArmiesAvailable(_ sender: Any) {
     }
     
+    func exibirPopUp() {
+        let alertController = UIAlertController(title: "Digite o número de territórios iniciais", message: nil, preferredStyle: .alert)
+        
+        alertController.addTextField { textField in
+            textField.placeholder = "Ex:15"
+            textField.keyboardType = .numberPad
+            NotificationCenter.default.addObserver(self, selector: #selector(self.handleTextFieldTextDidChangeNotification(_:)), name: UITextField.textDidChangeNotification, object: textField)
+        }
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            if let textField = alertController.textFields?.first, let texto = textField.text {
+                let valor = Int(texto) ?? 0
+                self.numberTerritories = valor
+                self.updateViewInformation()
+            }
+        }
+        okAction.isEnabled = false
+        
+        alertController.addAction(okAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
     
+    @objc func handleTextFieldTextDidChangeNotification(_ notification: Notification) {
+        if let alertController = presentedViewController as? UIAlertController,
+           let textField = alertController.textFields?.first,
+           let okAction = alertController.actions.first {
+            if let texto = textField.text, let valor = Int(texto) {
+                okAction.isEnabled = valor > 0
+            } else {
+                okAction.isEnabled = false
+            }
+        }
+    }
+    
+    func updateViewInformation() {
+        DispatchQueue.main.async {
+            self.labelTotalTerritoriesConquered.text = self.convertIntToString(number: self.numberTerritories ?? 0)
+            self.labelArmyAvailable.text = self.convertIntToString(number: (self.numberTerritories ?? 0)/2)
+        }
+    }
+    
+    func colorPlayer(number: Int) {
+        switch number {
+            case 0:
+                if let imagem = UIImage(named: "bola-branca") {
+                    imageViewColorPlayer.image = imagem
+                }
+            case 1:
+                if let imagem = UIImage(named: "bola-preta") {
+                    imageViewColorPlayer.image = imagem
+                }
+            case 2:
+                if let imagem = UIImage(named: "bola-verde") {
+                    imageViewColorPlayer.image = imagem
+                }
+            case 3:
+                if let imagem = UIImage(named: "bola-vermelha") {
+                    imageViewColorPlayer.image = imagem
+                }
+            case 4:
+                if let imagem = UIImage(named: "bola-azul") {
+                    imageViewColorPlayer.image = imagem
+                }
+            default:
+                if let imagem = UIImage(named: "bola-amarela") {
+                    imageViewColorPlayer.image = imagem
+                }
+        }
+    }
 }
